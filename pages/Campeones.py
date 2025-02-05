be41eba8ac8c3d9f1e5542f79cda7228a9ef80dc
Void-CA <castilloari282@gmail.com>
2025-02-04 21:06:25 -0600

    col faceting plot

--- pages/Campeones.py
+++ pages/Campeones.py
@@ -39,25 +39,26 @@ def show_champions():
 
 def compare_champions():
     cols = st.columns(2)
-    fms = utils.load_data("FMS")
+    fms = utils.load_data("Scaled")
     fms["year"] = fms["year"].replace({"2023A": "2023", "2023B": "2023"})
-    
-    with cols[0]:
-        # Selecciona un país
-        country = st.selectbox("Selecciona un país", fms["country"].sort_values().unique())
-        
-    # Filtrar los años disponibles para el país seleccionado
-    years_for_country = fms.query(f"country == '{country}'")["year"].sort_values().unique()
-    
-    with cols[1]:
-        # Selecciona un año basado en el país
-        year = st.selectbox("Selecciona un año", years_for_country)
-    
-    # Filtrar los datos para ese país y año
-    filtered_fms = fms[(fms["year"] == year) & (fms["country"] == country)]
-    
+    fms = fms.sort_values(["year", "PTS_scaled"], ascending=False)
+
     # Crear el gráfico
-    fig = px.scatter(filtered_fms, x="PTB", y="PTS", color="champion", title="Comparación de Puntos de los Campeones", color_discrete_map={0: "gray", 1: "gold"})
+    fig = px.scatter(fms, x="PTS_scaled", y="PTB_scaled", color="champion", 
+                     title="Comparación de Puntos de los Campeones", 
+                     color_discrete_map={0: "gray", 1: "gold"}, 
+                     facet_col="year", 
+                     facet_col_wrap=3,
+                     width=1200,
+                     custom_data=[fms["MC"], fms["year"], fms["champion"]]
+                     )
+    
+    fig.update_traces(
+        hovertemplate="<b>%{customdata[0]}</b><br>" +  # Nombre del MC
+                        "Campeón: %{customdata[2]}<br>" +  # Campeón
+                      "PTB: %{y:.2f}<br>" +  # PTB
+                      "PTS: %{x:.2f}<extra></extra>"  # PTS
+    )
     
     # Mostrar el gráfico
     st.plotly_chart(fig)
@@ -71,3 +72,13 @@ show_champions()
 st.write("Una pregunta para empezar, un campeón de FMS ¿es siempre el MC con más puntos?")
 st.write("Para responder a esta pregunta, analizaremos los campeones comparando sus puntos con los de los demas en MCs de esa misma temporada.")
 compare_champions()
+st.markdown("""
+         En el gráfico anterior, los campeones están marcados en dorado, mientras que los demás MCs están en gris.
+         Usamos los PTB y PTS escalados por año para comparar el desempeño relativo de los MCs. Los PTB miden el desempeño por batalla, 
+         los PTS miden los puntos obtenidos a traves de las victorias en su temporada. Mientras el punto este mas a la derecha
+         implicara que el MC obtuvo mas puntos en su temporada, mientras que si esta mas arriba implica que el MC obtuvo mas PTB en su temporada.
+         """)
+st.markdown(" #### Que podemos observar?")
+st.write("""
+         En el grafico anterior podemos observar
+         """)
